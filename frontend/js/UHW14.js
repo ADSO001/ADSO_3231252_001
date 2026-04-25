93db062e6c6b2c2fe5eee0fbd9def98d9aa3574a
Leandro <leandrocalvo16@utp.edu.co>
2026-04-25 15:47:47 -0500

    criteria 1. the header will have patient info and a button to log out with its js to work properly

---
+++ frontend/js/UHW14.js
@@ -0,0 +1,12 @@
+const botonCerrarSesion = document.getElementById('logout');
+
+    botonCerrarSesion.addEventListener('click', function() {
+
+        //Limpiar el almacenamiento del navegador
+        localStorage.removeItem('tokenUsuario');
+        localStorage.clear(); 
+
+        sessionStorage.clear(); //los datos se borran al cerrar la pestaña
+        
+        window.location.href = 'index.html'; //Redirigir al inicio de sesión
+    }); 
