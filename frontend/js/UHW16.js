9038ed4305c3ccf742769542119b41b1f1236d95
Dylan <dilanyesid1010@gmail.com>
2026-04-26 15:32:15 -0600

    1. I added the side menu

---
+++ frontend/js/UHW16.js
@@ -0,0 +1,7 @@
+function abrirPopUp() {
+    document.getElementById("ventanaEdicion").style.display = "block";
+}
+
+function cerrarPopUp() {
+    document.getElementById("ventanaEdicion").style.display = "none";
+}
