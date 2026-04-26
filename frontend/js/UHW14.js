f19d0513a47b3a650d3917e94f5a4d13b1ce9340
Leandro <leandrocalvo16@utp.edu.co>
2026-04-26 10:38:37 -0500

    Criteria 2. The website will display info about the amount of appointments for today, the week and taken ones.

--- frontend/js/UHW14.js
+++ frontend/js/UHW14.js
@@ -1,12 +1,12 @@
 const botonCerrarSesion = document.getElementById('logout');
 
-    botonCerrarSesion.addEventListener('click', function() {
+botonCerrarSesion.addEventListener('click', function () {
 
-        //Limpiar el almacenamiento del navegador
-        localStorage.removeItem('tokenUsuario');
-        localStorage.clear(); 
+    //Limpiar el almacenamiento del navegador
+    localStorage.removeItem('tokenUsuario');
+    localStorage.clear();
 
-        sessionStorage.clear(); //los datos se borran al cerrar la pestaña
-        
-        window.location.href = 'index.html'; //Redirigir al inicio de sesión
-    }); 
+    sessionStorage.clear(); //los datos se borran al cerrar la pestaña
+
+    window.location.href = 'index.html'; //Redirigir al inicio de sesión
+}); 
