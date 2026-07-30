f93e2c91aeba359ca3de46b05c63b4a41190d6d1
FenxxDev <dilanyesid1010@gmail.com>
2026-07-30 03:12:05 -0500

    First version; changes are still needed.

---
+++ helpers/emails.js
@@ -0,0 +1,65 @@
+import nodemailer from "nodemailer"
+
+const emailRegistro = async (datos) => {
+    try {
+        const transporter = nodemailer.createTransport({
+            host: process.env.EMAIL_HOST,
+            port: process.env.EMAIL_PORT,
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS,
+            },
+            connectionTimeout: 5000, // Tiempo máximo de espera: 5 segundos
+            socketTimeout: 5000
+        });
+
+        const { email, nombre, token } = datos
+
+        await transporter.sendMail({
+            from: "Bienes Raices SENA <noreply@miproyecto.com>",
+            to: email,
+            subject: "Confirma tu cuenta de SIGMI SENA",
+            text: "Confirma tu cuenta!",
+            html: `
+            <p>Hola ${nombre}, comprueba tu cuenta en SIGMI SENA</p>
+            <p>Tu cuenta ya está lista, solo debes confirmar en el siguiente enlace: <a href="${process.env.BACKEND_URL}:${process.env.PORT}/confirmar/${token}">Confirmar cuenta</a></p>
+            <p>Si no creaste la cuenta, omite este correo</p>
+            `
+        });
+    } catch (error) {
+        console.log("Error al enviar el correo de registro:", error);
+    }
+}
+
+const emailOlvidePassword = async (datos) => {
+    try {
+        const transporter = nodemailer.createTransport({
+            host: process.env.EMAIL_HOST,
+            port: process.env.EMAIL_PORT,
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS,
+            },
+            connectionTimeout: 5000,
+            socketTimeout: 5000
+        });
+
+        const { email, nombre, token } = datos
+
+        await transporter.sendMail({
+            from: "SIGMI SENA <noreply@miproyecto.com>",
+            to: email,
+            subject: "Restablecer contraseña de SIGMI SENA",
+            text: "Restablecer contraseña!",
+            html: `
+            <p>Hola ${nombre}, restaura tu contraseña en SIGMI SENA</p>
+            <p>Para cambiar la contraseña, solo debes seguir el siguiente enlace: <a href="${process.env.BACKEND_URL}:${process.env.PORT}/forgotPassword/${token}">Restablecer ahora</a></p>
+            <p>Si no la pediste, ignora el mensaje</p>
+            `
+        });
+    } catch (error) {
+        console.log("Error al enviar el correo de contraseña:", error);
+    }
+}
+
+export { emailRegistro, emailOlvidePassword }
--- routes/usuariosRoutes.js
+++ routes/usuariosRoutes.js
@@ -1,13 +1,27 @@
-import express from "express";
+    import express from "express";
 
-import {formularioLogin, formularioPaciente,formularioContraseña, patientPanel} from "../controllers/usuariosControllers.js";
+    import { formularioLogin, autenticar, patientPanel, registrar, confirmar, formularioRegistro, resetPassword, formularioOlvidePassword, comprobarToken, nuevaPassword } from "../controllers/usuariosControllers.js";
 
-const router = express.Router();
+    const router = express.Router();
 
-router.get("/login", formularioLogin)
-router.get("/pacientRegistration", formularioPaciente)
-router.get("/forgotPassword", formularioContraseña)
-router.get("/patientPanel", patientPanel)
+    router.get("/login", formularioLogin)
+    router.post("/login", autenticar);
+
+    router.get("/pacientRegistration", formularioRegistro)
+    router.post("/pacientRegistration", registrar)
+
+    router.get("/confirmar/:token", confirmar)
+
+    router.get("/forgotPassword", formularioOlvidePassword)
+    router.post("/forgotPassword", resetPassword)
+
+    // Validar el Olvide Contraseña
+    router.get("/forgotPassword/:token", comprobarToken)
+    router.post("/forgotPassword/:token", nuevaPassword)
+
+    router.get("/patientPanel", patientPanel)
 
 
-export default router;
+
+
+    export default router;
